4f0722784fd635ef419208f88a97318fe82c8b51
Youssef Ashraf <youssef42004@gmail.com>
2023-08-28 06:45:25 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* functions.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+
+/**
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: function
+ **/
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
+
+#endif
